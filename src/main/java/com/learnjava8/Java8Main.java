1e354ef0713e5b8888db75895bbe231dcb687f1c
Raja Bhaiya Vishwakarma <rajasvnit51@gmail.com>
2020-03-25 10:04:08 +0530

    Initial Commit.

---
+++ src/main/java/com/learnjava8/Java8Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.learnjava8;
+
+/**
+ * @author Raja Vishwakarma
+ *
+ */
+public class Java8Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
