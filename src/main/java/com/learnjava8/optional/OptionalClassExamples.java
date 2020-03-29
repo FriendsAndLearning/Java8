06ada42cff1ff3bd6b5798e06de905988a6b72ff
Raja Bhaiya Vishwakarma <rajasvnit51@gmail.com>
2020-03-29 19:55:26 +0530

    Update Java 8 Optional Class Examples

--- src/main/java/com/learnjava8/optional/OptionalClassExamples.java
+++ src/main/java/com/learnjava8/optional/OptionalClassExamples.java
@@ -1,6 +1,8 @@
 package com.learnjava8.optional;
 
 import java.util.Optional;
+import java.util.function.Supplier;
+
 
 /**
  * @author Raja Vishwakarma
@@ -32,6 +34,13 @@ public class OptionalClassExamples {
 		System.out.println("Sum : " + sum);
 
 		optionalClassExamples.createOptionalObjectExamples();
+		
+		optionalClassExamples.useOptionalToAvoidNullPointerException();
+		
+		optionalClassExamples.returnDefaultValueUsingOptional();
+		
+		optionalClassExamples.useFilterMethodWithOptional();
+
 	}
 
 	public Integer sum(Optional<Integer> a, Optional<Integer> b) {
@@ -57,6 +66,11 @@ public class OptionalClassExamples {
 		return value1 + value2;
 	}
 
+	private Person getPerson() {
+		return new Person("Raja Bhaiya", Optional.of(new Address("Narendra Nagar", "Nagpur", "India", 440015)),
+				"9730043");
+	}
+	
 	public void createOptionalObjectExamples() {
 
 		// 1. Create an empty Optional by using the static method
@@ -89,5 +103,85 @@ public class OptionalClassExamples {
 		Optional<Person> person2Optional = Optional.ofNullable(person2);
 		System.out.println("person2Optional : " + person2Optional);
 
+	}
+
+	public void useOptionalToAvoidNullPointerException() {
+		/*
+		 * Optional allows you to deal with the presence or absence of values
+		 * instead of doing a null check.
+		 */
+		
+		
+		/* Before Java 8:
+		 * 
+		 * Person p = new Person("Robin", new Address(block, city, state, country);
+		 * Address a = p.getAddress();
+		 * if(a != null){
+		 *  System.out.println(p);
+		 * }
+		 */
+
+		Person person = getPerson();
+		/*
+		 * In Java 8 you can completely avoid null check by using the
+		 * ifPresent() method of the Optional class, which allows you to execute
+		 * code if a value is present and the code will not execute if no value
+		 * is there.
+		 */	
+		Optional<Address> personOptional = person.getAddress();
+		personOptional.ifPresent(System.out::println);
+
+		/*
+		 * Btw, some Java programmer still uses the Optional like below: This is
+		 * not recommended because it is similar to classical check and not the
+		 * right way to use Optional in Java SE 8.
+		 */
+
+		if (!personOptional.isPresent()) {
+			System.out.println(personOptional.get());
+		}
+	}
+
+	public void returnDefaultValueUsingOptional() {
+
+		/*
+		 * To return a default value if Optional is empty i.e. doesn't contain a
+		 * value. You can use the Optional.orElse() method to return the default
+		 * value
+		 */
+		
+		Person person = new Person("Raja Bhaiya", Optional.ofNullable(null), "9730043");
+		Optional<Address> personOptional = person.getAddress();
+		Address address = personOptional.orElse(Address.EMPTY_ADDRESS);
+		System.out.println("Address : " + address);
+		
+		/*
+		 * You can also throw an exception if Optional doesn't contain any value
+		 * by using the Optional.orElseThrow() method as shown below: So, you
+		 * can choose whatever your situation demands.
+		 */
+		try {
+			Address address2 = person.getAddress().orElseThrow(new Supplier<Exception>() {
+				@Override
+				public Exception get() {
+					// TODO Auto-generated method stub
+					return new Exception("Address not found!");
+				}
+			});
+			System.out.println("Address2 : " + address2);
+		} catch (Exception e) {
+			System.err.println("Exception : " + e.toString());
+		}
+		
+	}
+	
+	public void useFilterMethodWithOptional() {
+
+		/*
+		 * Similar to the Stream class, Optional also provides a filter() method
+		 * to select or weed out unwanted values.
+		 */
+		Optional<Address> address = getPerson().getAddress();
+		address.filter(add -> "Nagpur".equals(add.getCity())).ifPresent(add -> System.out.println("Lives in Nagpur."));
 	}
 }
